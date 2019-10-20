40b91dfdad1fc79e5b7ae7aff55b3d7451bd14df
duRawan <du_rawan@outlook.com>
2019-10-20 10:07:38 +0300

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+
+
+app.use(express.static(__dirname + "/public/dist/public"));
+app.use(bodyParser.json());
+
+
+//mongoose.connect("mongodb://localhost/");
+//require("./server/config/mongoose.js");
+//require("./server/config/routes.js")(app);
+
+// this route will be triggered if any of the routes above did not match
+app.all("*", (req,res,next) => {
+    res.sendFile(path.resolve("./public/dist/public/index.html"))
+});
+
+app.listen(8000, function(){
+    console.log("Listening on part: 8000");
+})
